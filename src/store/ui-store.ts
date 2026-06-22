e1145cc491fc84469657baa47eeef9b27fafcedf
iammsp-star <manassubhash2007@gmail.com>
2026-06-22 09:54:48 +0530

    feat: add About OS dialog with GitHub and LinkedIn links

--- src/store/ui-store.ts
+++ src/store/ui-store.ts
@@ -4,17 +4,21 @@ interface UIState {
   sidebarOpen: boolean;
   sidebarCollapsed: boolean;
   quickCaptureOpen: boolean;
+  aboutOpen: boolean;
   toggleSidebar: () => void;
   setSidebarOpen: (open: boolean) => void;
   toggleSidebarCollapse: () => void;
   toggleQuickCapture: () => void;
   setQuickCaptureOpen: (open: boolean) => void;
+  toggleAbout: () => void;
+  setAboutOpen: (open: boolean) => void;
 }
 
 export const useUIStore = create<UIState>((set) => ({
   sidebarOpen: false,
   sidebarCollapsed: false,
   quickCaptureOpen: false,
+  aboutOpen: false,
   toggleSidebar: () => set((state) => ({ sidebarOpen: !state.sidebarOpen })),
   setSidebarOpen: (open) => set({ sidebarOpen: open }),
   toggleSidebarCollapse: () =>
@@ -22,4 +26,6 @@ export const useUIStore = create<UIState>((set) => ({
   toggleQuickCapture: () =>
     set((state) => ({ quickCaptureOpen: !state.quickCaptureOpen })),
   setQuickCaptureOpen: (open) => set({ quickCaptureOpen: open }),
+  toggleAbout: () => set((state) => ({ aboutOpen: !state.aboutOpen })),
+  setAboutOpen: (open) => set({ aboutOpen: open }),
 }));
